df38c901427fe94b0562c9e9383b30487dc9688d
inseong <twin2688@naver.com>
2022-06-15 16:19:45 +0900

    04: 19 최신본

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -15,8 +15,6 @@ const Detail = () => {
 
 
 
-  console.log(studyId);
-
   const comment_ref = React.useRef(null);
 
 
@@ -40,7 +38,29 @@ const Detail = () => {
 
 
 
+ const insert_btn = async() =>{
+  let title = prompt('제목을 수정할게요!')
+  let content = prompt('내용을 수정할게요 !')
+  let put_comment = {
+    'studyTitle': title,
+    'studyContent':content,
+    'studyAddress' : '지정안함'
+ };
 
+   await axios
+  .put("http://13.125.151.93/api/putstudy/" + studyId, put_comment ,{
+    headers: { Authorization: `${token}` },
+  }).then((res)=>{
+    alert('수정 성공')
+    window.location.reload()
+  }).catch((err)=>{
+    alert(err.response.data.message)
+    window.location.reload()
+  })
+
+
+
+ }
 
 
 
@@ -57,9 +77,7 @@ const Detail = () => {
       .get("http://13.125.151.93/api/getstudy/" + studyId) // back-end server http://13.125.151.93/api/poststudy
       .then((response) => {
         setList(response.data);
-
         console.log(response);
-        console.log(response.data)
         response.data.commentList.reverse();
 
       })
@@ -92,7 +110,6 @@ const Detail = () => {
       });
   };
 
-  console.log(list.commentList);
 
 
   
@@ -124,10 +141,10 @@ const Detail = () => {
           <div className="Detail_btn">
           {userName === now_user ? (
           <div>
-            <button>
+            <button onClick={insert_btn} className="btn_insert">
               수정
             </button>
-            <button  onClick={detail_del}>
+            <button  onClick={detail_del} className="btn_delete">
               삭제
             </button>
           </div>
